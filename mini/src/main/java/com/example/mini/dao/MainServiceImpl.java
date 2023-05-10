04545fb02265a3c529fe261a00703d691babcbb9
tj <tj@DESKTOP-BF6UOH1>
2023-05-10 14:54:30 +0900

    main mapper, service, servicImpl

---
+++ mini/src/main/java/com/example/mini/dao/MainServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.mini.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.mini.mapper.MainMapper;
+
+@Service
+public class MainServiceImpl implements MainService{
+
+	@Autowired
+	MainMapper mainMapper;
+
+
+}

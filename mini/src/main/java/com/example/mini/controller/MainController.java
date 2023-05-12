60ad0ce19050e4659d3c8f325e0e642ab0e3e9fc
tj <tj@DESKTOP-BF6UOH1>
2023-05-12 10:10:33 +0900

    MainController Login 연동, Logout 아직 안됨

--- mini/src/main/java/com/example/mini/controller/MainController.java
+++ mini/src/main/java/com/example/mini/controller/MainController.java
@@ -1,16 +1,26 @@
 package com.example.mini.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
 
 	
+	@Autowired
+	HttpSession session;
+	
 	@RequestMapping("/main.do") 
-    public String mainPage(Model model) throws Exception{
-
+    public String mainPage(HttpServletRequest request, Model model) throws Exception{
+		request.setAttribute("sessionId", session.getAttribute("sessionId"));
+		request.setAttribute("sessionName", session.getAttribute("sessionName"));
+		request.setAttribute("sessionStatus", session.getAttribute("sessionStatus"));
+		
         return "/main";
     }
 	
